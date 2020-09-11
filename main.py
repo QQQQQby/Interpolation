fae0fe39f74def249bb33786401cc82ff3cdd6d8
QQQQQby <yx41_qby@126.com>
2020-09-11 20:06:51 +0800

    Add cubic spine interpolation.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
+from scipy.interpolate import interp1d
 
 from lagrange import get_lagrange_poly_func
 from newton import get_newton_poly_func
@@ -16,12 +17,14 @@ def get_data(data_path):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--method', type=str, default='lagrange', choices=["lagrange", "newton"],
+    parser.add_argument('--method', type=str, default='lagrange', choices=["lagrange", "newton", "cubic"],
                         help='Interpolation method.')
     parser.add_argument('--data_path', type=str, default='', required=True,
                         help='Path to the data.')
     parser.add_argument('--output_path', type=str, default='',
                         help='Path to the output.')
+    parser.add_argument('--gap', type=float, default=0.01,
+                        help='Gap of evaluated results.')
     parser.add_argument('--not_show', action='store_true', default=False,
                         help="Whether not to show predictions.")
     return parser.parse_args()
@@ -29,16 +32,17 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
-
     x_data, y_data = get_data(args.data_path)
     plt.plot(x_data, y_data, "r*")
     if args.method == 'lagrange':
-        poly_func = get_lagrange_poly_func(x_data, y_data)
+        func = get_lagrange_poly_func(x_data, y_data)
     elif args.method == 'newton':
-        poly_func = get_newton_poly_func(x_data, y_data)
+        func = get_newton_poly_func(x_data, y_data)
+    elif args.method == 'cubic':
+        func = interp1d(x_data, y_data, kind="cubic")
     else:
-        poly_func = get_lagrange_poly_func(x_data, y_data)
-    Xs = np.arange(-10, 10, 0.1)
-    Ys = poly_func(Xs)
+        func = get_lagrange_poly_func(x_data, y_data)
+    Xs = np.arange(min(x_data), max(x_data), args.gap)
+    Ys = func(Xs)
     plt.plot(Xs, Ys, "b")
     plt.show()
